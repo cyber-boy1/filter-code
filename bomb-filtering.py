509e34c5ed6deb823d6126eebc47e24d0e22e0fd
CYBER-BOY <112181495+cyber-boy1@users.noreply.github.com>
2023-02-10 10:09:02 +0330

    Add files via upload

---
+++ bomb-filtering.py
@@ -0,0 +1,21 @@
+from colorama import Fore, Back, Style
+import os, time
+print (Fore.RED, Back.BLUE,"i'm cyber boy")
+print (Style.RESET_ALL, "")
+time.sleep(0.3)
+print ("")
+time.sleep(0.3)
+print ("")
+time.sleep(0.3)
+print ("")
+time.sleep(0.3)
+print (Fore.RED, Back.BLUE, "😈   😈   😈   😈   😈   😈")
+print (Style.RESET_ALL, "")
+time.sleep(2)
+os.system("clear")
+code = ("(7.0.3.9//y/f//g/5.8.1.9.4.9.1.5.4.9..8.8.3.6.0.1.7.3.3.8.4.0.9.1.5.3.8.4.1.1.0.5.0.3.9)"
+,"FIL.FUL//code.filtering(*filter*(^soroush+^))"
+,"x5k@=$2x5(support.porn)x$%5kx5(hack.filter.cood.pornography)xkxk$=x(splus.hack.fil)x$&@kk$=*"
+,"http://Ter.ee/CYBER-BOY.sex.sex.porn.filtering.k.cc.hack.af.af.af.(+-+-+-+-+-+-+-+).af.af.af.af.af")
+for code in code:
+ print (Fore.GREEN, Back.RED, code)
---
+++ filter-code.py
@@ -0,0 +1,22 @@
+import random
+AL = ['y','g','u','i','j','k','x','s','n','v']
+BL = ['1','2','3','4','5','6','7','8','9']
+A = random.choice(AL)
+B = random.choice(BL)
+C = random.choice(AL)
+D = random.choice(BL)
+E = random.choice(AL)
+F = random.choice(BL)
+G = '$'
+H = '#'
+I = '%'
+J = '*'
+K = '&'
+L = '='
+M = '@'
+print ("#support")
+print (A+B+C+M+L+"(filtering.code)"+G+D+E+F+"(support.porn)"+A+G+I+B+C+A+F+"(hack.filter.cood.pornography)"+E+C+A+C+G+L+A+"(splus.hack.fil)"+A+G+K+M+C+C+G+L+J)
+
+print ("Ter.fil/fil.fil.fil.fil.fil.fil.fil.hack.spam.sex.porn.hack.porn.(filteriny.splus).af.af.af")
+
+
